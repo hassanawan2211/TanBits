87ad1270af6f48f446dec496f5bfc698fddeb643
hassanawan2211 <ali.hassan@tanbits.org>
2023-12-28 11:07:43 +0500

    practice work related to Arithmetic operators

---
+++ index3.js
@@ -0,0 +1,102 @@
+// Arithmetics operators 
+
+
+// Addition
+var a = 20;
+var b =  30;
+
+let c = a + b;
+
+document.write(c, "<br>");
+
+// Subtraction
+
+var y = 20;
+var s =  30;
+
+let z = s - y;
+
+document.write(z, "<br>");
+
+
+// multiplication 
+
+
+var num1 = 20;
+var num2 =  30;
+
+let total = num2 * num1;
+
+document.write(total, "<br>");
+
+
+// division
+
+
+var k = 20;
+var l =  30;
+
+let t = l / k ;
+
+document.write(t, "<br>");
+
+
+// modulus
+
+
+var num3 = 20;
+var num4 =  30;
+
+let remainder = num4 % num3 ;
+
+document.write(remainder, "<br>");
+
+
+// Exponentiation  ** 
+
+// used  for power 
+
+
+var A = 2;
+var B = 3;
+
+let C = A ** B;
+
+document.write(C, "<br>");
+
+// Increment
+
+var D = 2;
+var E =  30;
+
+document.write(D + E);
+document.write( "<br>");
+
+D++;
+
+document.write(D + E, "<br>");
+
+// decrement
+
+var M = 2;
+var O =  5;
+
+document.write(M + O);
+document.write( "<br>");
+
+M--;
+
+document.write(M + O, "<br>");
+
+
+// use different operatore 
+
+ let number = 5;
+ let number2 = 5;
+
+ let total_number = number + number2 * 4;
+
+ document.write(total_number);
+
+ 
+
