cd8c6b18567201b7d197a03510e452df21706a10
hassanawan2211 <ali.hassan@tanbits.org>
2023-12-28 15:33:46 +0500

    practice work for Assignment operators and google chrome   console

---
+++ index4.js
@@ -0,0 +1,66 @@
+// Assignment operators 
+
+// Assignment operators with addition 
+
+// let a = 10;
+// let b = 20;
+
+// a += b;
+
+// document.write(a);
+
+
+// Assignment operators with Subtraction
+
+let a = 10;
+let b = 20;
+
+a -= b;
+
+document.write(a);
+
+// also do with other operators like modulud, division etc;
+
+
+
+// google console 
+
+
+let name = "Tanbits";
+let city = "Lahore";
+console.log(name , city);
+
+
+
+console.log([3,3,4,3]);
+
+
+// for checking error also check error in condition we use console.error
+ 
+
+console.error("Something went worng ");
+
+
+// warn use for show any warning msg etc with show warning sign also
+console.time("test");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+console.warn("This is just Warning ");
+
+console.timeEnd("test");
+// console clear is used for for console section 
+
+// console.clear("console section is cleare ");
+
+// timeend used for checking runing time for the commands 
+
+console.timeEnd("test");
+
+// time show in ms 
+
+
+
