bbc639be8857a9bc14b95484254c294cda3458c6
hassanawan2211 <ali.hassan@tanbits.org>
2023-12-28 16:26:57 +0500

    practice work related to comparison operators

---
+++ index5.js
@@ -0,0 +1,103 @@
+// == by using this type operators 
+
+let a = 2;
+
+let b = 20;
+
+console.log( a == b );
+
+// this type of operator check only value
+let A = 20;
+
+let B = "20"
+
+console.log(A == B);
+
+
+// === used for checking both data type and value 
+
+
+let num = 56;
+
+let num1 = "56"
+
+console.log(num === num1);
+
+
+let Name = "Ali";
+let last_name = "Ali"
+
+console.log(Name === last_name);
+
+
+
+// for not equal 
+
+
+let num2 = 6;
+
+let num3 = "5";
+
+console.log(num2 != num3);
+
+// for not equal value or nor equal type is used !== 
+
+
+let num4 = "ali";
+
+let num5 = "ali";
+
+console.log(num4 !== num5);
+
+
+
+// greater than
+
+let p = 4;
+let q = 5;
+
+console.log(p > q);
+
+
+// less than 
+
+
+let y = 6;
+
+let z = 8;
+
+console.log(y < z);
+
+// less than or equal to 
+
+let number = 5;
+
+let number1 = 5;
+
+console.log(number <= number1);
+
+
+// greater than or equal to 
+
+
+let mark = 987;
+
+let total = 1100;
+
+console.log(mark >= total);
+
+
+//compaire null and undefine data type with operators 
+
+// by using == 
+console.log(null == undefined);
+
+
+// by using ===
+console.log(null === undefined);
+
+
+
+
+
+
