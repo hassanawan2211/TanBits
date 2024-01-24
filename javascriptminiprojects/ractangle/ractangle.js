91b15d01cd1a7c1902327c2389640565dcf6b47c
hassanawan2211 <ali.hassan@tanbits.org>
2024-01-24 14:55:08 +0500

    reactangular color change functionailty with gsap done

--- javascriptminiprojects/ractangle/ractangle.js
+++ javascriptminiprojects/ractangle/ractangle.js
@@ -1,5 +1,37 @@
 var rect = document.querySelector("#center");
 
-rect.addEventListener("mousemove", function(){
-    console.log("hi")
+rect.addEventListener("mousemove", function(details){
+    var rectangalLocation = rect.getBoundingClientRect();
+    var insidRectValue =details.clientX - rectangalLocation.left;
+
+    if(insidRectValue < rectangalLocation.width/2){
+        var grayColor = gsap.utils.mapRange(0,
+            rectangalLocation.width/2, 255,
+            0,
+            insidRectValue
+            );
+            gsap.to(rect,{
+                backgroundColor: `rgb(${grayColor},88,88)`,
+                ease: Power4,
+            });
+    }else{
+        var greenColor = gsap.utils.mapRange(
+
+            rectangalLocation.width / 2 ,
+            rectangalLocation.width,
+             0,
+            255,
+            insidRectValue
+            );
+            gsap.to(rect,{
+                backgroundColor: `rgb(0,128,${greenColor})`,
+                ease: Power4,
+            });
+    }
+})
+
+rect.addEventListener("mouseleave" , function(){
+    gsap.to(rect, {
+        backgroundColor: "white"
+    })
 })
