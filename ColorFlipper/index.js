0dea31ff571081a7fa8a0907cf2d378151658948
hassanawan2211 <ali.hassan@tanbits.org>
2024-01-24 16:22:16 +0500

    color flipper page done

---
+++ ColorFlipper/index.js
@@ -0,0 +1,15 @@
+const colorArray = ["blue", "green", "gray", "pink", "red", "orange", "black", "white"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector('.color')
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colorArray[randomNumber];
+    color.innerHTML = colorArray[randomNumber];
+});
+
+function getRandomNumber (){
+    return Math.floor(Math.random() * colorArray.length);
+}
